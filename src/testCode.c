f1caa792e02560c44060da15979998e82530aaa9
Trent Nakata <naka0860@pacificu.edu>
2021-09-09 16:05:51 -0700

    initial commit

---
+++ src/testCode.c
@@ -0,0 +1,25 @@
+/****************************************************************************
+ File name:	HelloWorld.c
+ Author:     chadd williams
+ Date:       9/9/2021
+ Class:      CS300
+ Assignment: C Examples
+ Purpose:    Simple C Demo
+ ****************************************************************************/
+
+#include <stdio.h>
+
+/****************************************************************************
+ Function:			main
+
+ Description:	Display hello world
+
+ Parameters:		None
+
+ Returned:			0 for Success
+ ****************************************************************************/
+int main ()
+{
+	printf ("Hello World, I am Trent");
+	return 0;
+}
